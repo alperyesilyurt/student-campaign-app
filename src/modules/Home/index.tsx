ec5682c746217c0f6341610f3fa4998bbecd8577
bedirhan <celayirbedirhan@gmail.com>
2022-10-23 13:00:08 +0300

    chore: add button for trying

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -1,7 +1,8 @@
+import Button from "@/components/styled/button/Button";
 import React from "react";
 
 type Props = {};
 
-export default function Home({}: Props) {
-  return <div>Home page</div>;
+export default function Home({ }: Props) {
+  return <div>Home page <Button outlined={true} type="primary">bu bir styled button</Button></div>;
 }
