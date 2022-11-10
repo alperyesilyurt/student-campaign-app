e3f3ac6343a0068273e56fc954caed687150a5a8
bedirhan <celayirbedirhan@gmail.com>
2022-11-10 23:55:25 +0300

    feat: add campaign card

--- src/components/CampaignCard.tsx
+++ src/components/CampaignCard.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { lazy } from "react";
 import styled from "styled-components";
+import Button from "./styled/button/Button";
+import {
+  fontWeights,
+  boxSizes,
+  fontFamilies,
+  spacings,
+  borderRadiusSizes,
+  maxWidth,
+} from "./styled/constants";
+import * as colors from "./styled/colors";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 export type Campaign = {
   name?: string;
@@ -14,24 +26,70 @@ type Props = {
   campaign: Campaign;
 };
 const CampaignCardWrapper = styled.div`
-  background-color: #00000013;
-  padding: 0.6rem 0.4rem;
-  border-radius: 0.25rem;
+  font-family: ${fontFamilies.poppins}, sans-serif;
+  background-color: ${colors.white};
+  padding: ${spacings.large};
+  max-width: ${maxWidth.medium};
+  border-radius: ${borderRadiusSizes.medium};
   display: flex;
-  gap: 20px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const CampaignCardIndex = styled.div`
+  background-color: ${colors.white};
+  max-width: ${maxWidth.small};
+  margin-top: -30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: ${spacings.xsmall};
+  justify-content: space-around;
+`;
+
+const DescriptionContainer = styled.div`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  line-clamp: 3;
+  -webkit-box-orient: vertical;
+  text-align: center;
 `;
 
 function CampaignCard(props: Props) {
   const { campaign } = props;
 
   return (
-      <CampaignCardWrapper>
-        <img width={200} src={campaign.campaignHeroImage} />
-        <div>
-          <h3>{campaign.company.name}</h3>
-          <p>{campaign.validUntil}</p>
-        </div>
-      </CampaignCardWrapper>
+    <CampaignCardWrapper>
+      <LazyLoadImage
+        width={288}
+        src={campaign.campaignHeroImage}
+        effect="blur"
+        placeholderSrc={campaign.campaignHeroImage}
+      />
+      <CampaignCardIndex>
+        <LazyLoadImage
+          width={90}
+          height={90}
+          src={campaign.company.logo}
+          effect="blur"
+        />
+        <h3>{campaign.company.name}</h3>
+        <DescriptionContainer>
+          <p>{campaign.description}</p>
+        </DescriptionContainer>
+      </CampaignCardIndex>
+      <Button
+        variant="primary"
+        outlined
+        fontWeight={fontWeights.bold}
+        boxSize={boxSizes.xlarge}
+      >
+        Use
+      </Button>
+    </CampaignCardWrapper>
   );
 }
 
