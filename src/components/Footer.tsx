16b6c2406ca0b763f0fd7ddfcaa694a7b09180cb
Alper Yesilyurt <alperesilyurt@hotmail.com>
2022-11-11 21:09:13 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,8 +8,8 @@ type Props = {};
 const FooterWrapper = styled.div`
   background: #000000;
   width: 100%;
-  height: 20vh;
-  position:"relative";
+  height: 40vh;
+  position: "relative";
   bottom: 0;
   display: flex;
   flex-direction: row;
@@ -18,7 +18,6 @@ const FooterWrapper = styled.div`
 `;
 const Wrapper = styled.div`
   display: flex;
-  height: 20vh;
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
@@ -29,35 +28,58 @@ export default function Footer({}: Props) {
     <FooterWrapper>
       <Wrapper>
         <UniLifeLogo_2 />
-        <Button variant="link" size="small" >
+        <Button variant="link" size="small">
           Make your university life a lot cheaper and funnier
         </Button>
       </Wrapper>
       <Wrapper>
-      <Button variant="link" size="small" >
-      Popular Categories
+        <Button variant="link" size="small">
+          Popular Categories
         </Button>
-        <Button variant="link" size="small" >
-      Food
+        <Button variant="link" size="small">
+          Food
         </Button>
-        <Button variant="link" size="small" >
-      Fashion
+        <Button variant="link" size="small">
+          Fashion
         </Button>
-        <Button variant="link" size="small" >
-      Tech
+        <Button variant="link" size="small">
+          Tech
         </Button>
-        <Button variant="link" size="small" >
-      Health
+        <Button variant="link" size="small">
+          Health
         </Button>
-        <Button variant="link" size="small" >
-      Sports
+        <Button variant="link" size="small">
+          Sports
         </Button>
-        <Button variant="link" size="small" >
-      All
+        <Button variant="link" size="small">
+          All
         </Button>
-        </Wrapper>
-      <Wrapper>Corporate</Wrapper>
-      <Wrapper>Follow us</Wrapper>
+      </Wrapper>
+      <Wrapper>
+        <Button variant="link" size="small">
+          Corporate
+        </Button>
+        <Button variant="link" size="small">
+          About UniLife
+        </Button>
+        <Button variant="link" size="small">
+          Contact
+        </Button>
+        <Button variant="link" size="small">
+          Terms of Service
+        </Button>
+        <Button variant="link" size="small">
+          Cookie Policy
+        </Button>
+        <Button variant="link" size="small">
+          Privacy Policy
+        </Button>
+      </Wrapper>
+      <Wrapper>
+        <Button variant="link" size="small">
+          Follow us
+        </Button>
+      </Wrapper>
     </FooterWrapper>
   );
 }
