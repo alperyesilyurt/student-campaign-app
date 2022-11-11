58a88242d807a020fde3d22a779f4c0910a601a9
bedirhan <celayirbedirhan@gmail.com>
2022-11-11 12:07:27 +0300

    stlye: linted

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -3,6 +3,13 @@ import React from "react";
 
 type Props = {};
 
-export default function Home({ }: Props) {
-  return <div>Home page <Button outlined={true} type="primary">bu bir styled button</Button></div>;
+export default function Home({}: Props) {
+  return (
+    <div>
+      Home page
+      <Button outlined={true} variant="primary">
+        bu bir styled button
+      </Button>
+    </div>
+  );
 }
